5d439d544f8128bb9ee7d231d4e5e97e6d3fc88a
Alex Rubio <alejandro@flykube.com>
2022-10-29 23:20:22 +0200

    fix GiftList component

--- src/components/GifsList.jsx
+++ src/components/GifsList.jsx
@@ -1,29 +1,27 @@
-// import { useEffect, useState } from 'react';
-// import getNewGifsOBject from './components/getNewGifsOBject';
+import { useEffect, useState } from 'react';
+import getNewGifsOBject from './getNewGifsOBject';
 
+const GifsList = () => {
+  const [gifs, setGifs] = useState([]);
 
-// const [gifs, setGifs] = useState([]);
-// useEffect(function () {
-// 	getNewGifsOBject({ keyword: 'rick' }).then((gifs) => setGifs(gifs));
-// }, []);
+  useEffect(() => {
+    getNewGifsOBject({ keyword: 'rick' }).then((gifs) => setGifs(gifs));
+  }, []);
 
-const GifsList = ({ gifs = [] }) => {
-	return (
-		<div>
-			{gifs.map((setGifs) => {
-				return (
-					<div>
-						<h4>{setGifs.title}</h4>
-						<p>{setGifs.id}</p>
-						<p>{setGifs.hash}</p>
-						<img src={setGifs.url} alt="panda" />
-					</div>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div>
+      {gifs.map((setGifs) => {
+        return (
+          <div>
+            <h4>{setGifs.title}</h4>
+            <p>{setGifs.id}</p>
+            <p>{setGifs.hash}</p>
+            <img src={setGifs.url} alt="panda" />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
-
-
 
 export default GifsList;
